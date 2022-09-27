f1d8f9845420201e13bff9b130c02f86f3353d77
Jennifer Alvarez <jennifer.s.alvarez93@gmail.com>
2022-09-26 21:32:45 -0300

    se agrega card

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { Container } from "react-bootstrap";
 import Button from 'react-bootstrap/Button';
 import './style.css';
+import titulo from "./img/los simpsons.png";
+import Frase from "./components/Frase";
 
 function App() {
   return (
     <Container className='my-3 text-center'>
-      <h1 className="text-center">simpsons</h1>
-      <Button type="submit" className="button">Obtener frase</Button>
+      <img src={titulo}/>
+      <br/>
+      <Button type="submit" className="button my-4">Obtener frase</Button>
+      <Frase></Frase>
     </Container>
   );
 }
