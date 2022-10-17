c30265633bf40320d11585e20861454fa2135a50
Jennifer Alvarez <jennifer.s.alvarez93@gmail.com>
2022-10-17 12:24:55 -0300

    agregamos consulta a api

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,45 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Container } from "react-bootstrap";
-import Button from 'react-bootstrap/Button';
+import { Button, Container } from "react-bootstrap";
 import './style.css';
 import titulo from "./img/los simpsons.png";
 import Frase from "./components/Frase";
+import { useEffect, useState } from "react";
 
-function App() {
-  return (
-    <Container className='my-3 text-center'>
-      <img src={titulo}/>
-      <br/>
-      <Button type="submit" className="button my-4">Obtener frase</Button>
-      <Frase></Frase>
-    </Container>
-  );
-}
+
+ function App() {
+  const [personaje,setPersonaje] = useState({});
+
+  useEffect(()=>{
+    consultarApi();
+  },[])
+
+  const consultarApi = async()=>{
+    try {
+      const respuesta = await fetch('https://thesimpsonsquoteapi.glitch.me/quotes')
+      console.log(respuesta)
+      const dato = await respuesta.json();
+      console.log(dato[0])
+      setPersonaje(dato[0]);
+      
+    } catch (error) {
+      console.log(error);
+      //mostrar un mensaje al usuario
+    }
+  }
+
+    return (
+      <Container>
+        <div className="m-5 text-center">
+        <img src={titulo} alt="homero simpsons" className="w-100"/>
+        </div>
+        <div className="text-center">
+          <Button type="submit" className="button mb-5" variant="warning">Obtener frase</Button>
+        </div>
+        <Frase personaje={personaje}></Frase>
+      </Container>
+    );
+  }
+
+
 
 export default App;
--- src/components/Frase.jsx
+++ src/components/Frase.jsx
@@ -1,20 +1,27 @@
 import React from "react";
-import Card from "react-bootstrap/Card";
+import { Card, Row, Col } from "react-bootstrap";
 import Homero from "../img/homero.png";
 
 const Frase = () => {
   return (
-    <Card className="card mb-3" style={{ width: "100%" }}>
-        <div className="d-flex">
-         <Card.Img variant="top" src={Homero} />
-         <Card.Body>
-           <Card.Title>Homer Simpson</Card.Title>
-           <Card.Text>
-             Some quick example text to build on the card title and make up the
-             bulk of the card's content.
-           </Card.Text>
-         </Card.Body>
-        </div>
+    <Card>
+      <Card.Body>
+      <Row>
+        <Col md={4}>
+          <img src={Homero} alt='homero' className="w-100"/>
+        </Col>
+        <Col md={8}>
+          <Card.Title>
+            <h3>Homer Simpsons</h3>
+          </Card.Title>
+          <Card.Text>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi
+            dolorum, facilis ducimus recusandae exercitationem nemo eum
+            laudantium laboriosam culpa harum.
+          </Card.Text>
+        </Col>
+      </Row>
+      </Card.Body>
     </Card>
   );
 };
